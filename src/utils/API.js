2200c5368609e92a6226718cf017c8be889cecdb
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2024-06-17 09:11:46 +0530

    added backend

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,1 +1,1 @@
-export const mediaAPIString = `https://9ee8-2409-40e3-54-22b4-fd7d-5086-1df-42a5.ngrok-free.app/api/media`;
+export const mediaAPIString = `https://backend-gallery-app.onrender.com/api/media`;
